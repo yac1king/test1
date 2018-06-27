2f2dbbfe1575e9e26bf83489a034ef2bf38fec21
yac1king <40617080+yac1king@users.noreply.github.com>
2018-06-27 11:31:59 +0800

    0.0.1a testAddFile

---
+++ addstu.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>新增資料</title>
+<style type="text/css">
+   body {font-size: 24px;}  
+</style> 
+</head>
+<body>
+<?php 
+	header("Content-Type: text/html; charset=utf-8");
+
+	$dbname = "class";
+	$db_link = mysqli_connect("localhost", "root", "1234");
+	$seldb=@mysqli_select_db($db_link,$dbname);
+	
+	
+	if (!$seldb)die("資料庫切換失敗！");
+	//使用中文要用這兩行=========================================
+	mysqli_query($db_link, "SET CHARACTER SET utf8");
+	mysqli_query($db_link, "SET collation_connection = 	'utf8_general_ci'");
+	//=================================================
+	//$sql_query = "select id from students";
+	//$result = mysqli_query($db_link, $sql_query);
+	//$row_result=mysqli_num_rows($result);
+	//print_r($row_result);
+	//while($row_result=mysqli_fetch_assoc($result)){
+	$result= mysqli_query($db_link, "select id from students");
+	echo mysqli_num_rows($result);
+	$newid = mysqli_num_rows($result)+1;
+	$sql_query = "INSERT INTO students(id,name,sex,birth,email,phone,addr) VALUES($newid,'同學','M','1987-07-01','aaa@bbb.ccc','0912345678','aaa')";
+	mysqli_query($db_link, $sql_query) or die("無法新增".mysqli_connect_error());
+	
+?>
+</body>
+</html>
